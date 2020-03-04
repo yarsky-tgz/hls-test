5400d2ff2f412307192819fc1ba4b81bf339192f
Yaroslav Dobzhanskij <merlyn.tgz@gmail.com>
2020-03-04 12:46:35 +0200

    using fast-stats library, better output, more metrics and events from new fetch-video v1.1.0

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const term = require( 'terminal-kit' ).terminal;
 const { download } = require('fetch-video');
 const prettyBytes = require('pretty-bytes');
-const percentile = require('stats-percentile');
+const Stats = require('fast-stats').Stats;
 
 const currentTime = () => ((new Date()).getTime() / 1000);
 const FULL_PERCENTILES = [1, 5, 10, 50, 90, 95, 99];
@@ -27,12 +27,11 @@ function echoLine(line, text, color = 'defaultColor') {
     .previousLine(line)
 }
 const hlsStats = {
-  segmentSpeeds: [],
-  segmentTimes: [],
-  segmentSizes: [],
-  speedPercentiles: {},
-  timePercentiles: {},
-  sizePercentiles: {},
+  speedStats: new Stats(),
+  timeStats: new Stats(),
+  sizeStats: new Stats(),
+  segmentWaitStats: new Stats(),
+  playlistsLoadStats: new Stats(),
   lastSegment: {
     time: 0,
     size: 0,
@@ -40,43 +39,52 @@ const hlsStats = {
   }
 };
 const {
-  segmentSizes, segmentSpeeds, segmentTimes, speedPercentiles, sizePercentiles, timePercentiles, lastSegment
+  lastSegment, playlistsLoadStats, segmentWaitStats, sizeStats, speedStats, timeStats,
 } = hlsStats;
 const { argv: [, , url, refreshPeriod] } = process;
 const isLive = !!refreshPeriod;
 const refreshPeriodTime = parseInt((refreshPeriod || '0'), 10);
-let totalSegments = 0;
-let totalTransferred = 0;
-let totalSpeed = 0;
 const startTime = currentTime();
+let totalSegments = 0;
 let totalTime = 0;
-let totalDownloadTime = 0;
+let totalErrors = 0;
 const showStats = ({ speed, progress, transferred, segments, progressBar }) => {
   const { time, speed: segmentSpeed, size } = lastSegment;
   if (progressBar) progressBar.update(progress / 100);
   term
     .previousLine(1).column(1).eraseLine()
-    .yellow(`Speed: `).white(prettyBytes(isLive ? totalSpeed : speed))
-    .forwardTab()
+    .brightWhite('Total').forwardTab().brightWhite('| ')
     .green(`Segments: `).white(`${isLive ? totalSegments : segments.transferred}`).green('/').gray(`${isLive ? '-' : segments.length}`)
     .forwardTab()
-    .blue('Transferred: ').gray(prettyBytes(isLive ? totalTransferred : transferred))
+    .blue('Transferred: ').gray(prettyBytes(isLive ? sizeStats.sum : transferred))
     .forwardTab()
-    .cyan('Total time: ').gray(Math.floor(totalTime))
-    .previousLine(10).column(1).eraseLine()
+    .cyan('Test time: ').gray(Math.floor(totalTime))
+    .forwardTab()
+    .red('Errors: ').white(totalErrors.toString())
+    .previousLine(1).column(1).eraseLine()
+    .brightWhite('Average').forwardTab().brightWhite('| ')
+    .yellow(`Speed: `).white(prettyBytes(Math.round(speedStats.amean() || 0)))
+    .forwardTab()
+    .gray('Playlists time: ').white(playlistsLoadStats.amean().toFixed(3))
+    .forwardTab()
+    .magenta('Response time: ').white(segmentWaitStats.amean().toFixed(3))
+    .forwardTab()
+    .cyan('Segment download time: ').white(timeStats.amean().toFixed(3))
+    .previousLine(3 + FULL_PERCENTILES.length).column(1).eraseLine()
     .brightCyan(isLive ? '(LIVE!) ' : '').green('Last segment stats')
     .nextLine().column(1).eraseLine()
     .brightWhite('time:').forwardTab().defaultColor(`${time.toFixed(3)}, `)
     .brightWhite('speed:').forwardTab().defaultColor(`${prettyBytes(segmentSpeed)}, `)
     .brightWhite('size:').forwardTab().defaultColor(prettyBytes(size))
     .nextLine().column(1).eraseLine()
-    .green('Speed').forwardTab(3).green('Time').forwardTab(3).green('Size');
+    .green('Speed').forwardTab(3).green('Playlists time').forwardTab(2).green('Response time').forwardTab(2).green('Segment download time');
   FULL_PERCENTILES.forEach(percents => term
     .nextLine().column(1).eraseLine()
-    .white(`${percents}%`).forwardTab()[PERCENTS_COLORS[percents]](prettyBytes(speedPercentiles[`_${percents}`] || 0)).forwardTab(2)
-    .white(`${percents}%`).forwardTab()[PERCENTS_COLORS[percents]]((timePercentiles[`_${percents}`] || 0).toFixed(3)).forwardTab(2)
-    .white(`${percents}%`).forwardTab()[PERCENTS_COLORS[percents]](prettyBytes(sizePercentiles[`_${percents}`] || 0)));
-  term.nextLine(2);
+    .white(`${percents}%`).forwardTab()[PERCENTS_COLORS[percents]](prettyBytes(speedStats.percentile(percents) || 0)).forwardTab(2)
+    .white(`${percents}%`).forwardTab()[PERCENTS_COLORS[percents]]((playlistsLoadStats.percentile(percents) || 0).toFixed(3)).forwardTab(2)
+    .white(`${percents}%`).forwardTab()[PERCENTS_COLORS[percents]]((segmentWaitStats.percentile(percents) || 0).toFixed(3)).forwardTab(2)
+    .white(`${percents}%`).forwardTab()[PERCENTS_COLORS[percents]]((timeStats.percentile(percents) || 0).toFixed(3)));
+  term.nextLine(3);
 };
 const start = (url) => {
   const downloader = download(url);
@@ -86,23 +94,15 @@ const start = (url) => {
     eta: true,
     width: 60,
   });
+  downloader.on('playlistsLoad', ({ time }) => playlistsLoadStats.push(time));
+  downloader.on('response', ({ time }) => segmentWaitStats.push(time));
   downloader.on('lastSegmentStats', (lastSegmentStats) => {
     Object.assign(lastSegment, lastSegmentStats);
     const { time, speed, size } = lastSegment;
-    if (isLive) {
-      totalSegments++;
-      totalTransferred += size;
-      totalDownloadTime += time;
-      totalSpeed = Math.round(totalTransferred / totalDownloadTime);
-    }
-    const targets = [segmentTimes, segmentSpeeds, segmentSizes];
-    const percentiles = [timePercentiles, speedPercentiles, sizePercentiles];
+    if (isLive) totalSegments++;
+    const targets = [timeStats, speedStats, sizeStats];
     const sources = [time, speed, size];
-    sources.forEach((source, i) => {
-      const target = targets[i];
-      target.push(source);
-      Object.assign(percentiles[i], FULL_PERCENTILES.reduce(getPercentileReducer(target), {}));
-    });
+    sources.forEach((source, i) => targets[i].push(source));
   });
   downloader.on('stats', (stats) => {
     if (progressBar) Object.assign(stats, { progressBar });
@@ -114,13 +114,13 @@ const start = (url) => {
       if (progressBar) progressBar.update(1);
       if (!isLive) term("\n\n");
     })
-    .catch(e => console.log(e));
+    .catch(() => totalErrors++);
 };
-term.defaultColor("\n\n\n\n\n\n\n\n\n\n\n");
+term.defaultColor("\n\n\n\n\n\n\n\n\n\n\n\n\n");
 showStats({ speed: 0, progress: 0, transferred: 0, segments: { transferred: 0, length: Infinity } });
 const showTillNextUpdate = (timeLeft, step = 100) => {
   term.column(1).eraseLine().brightCyan(timeLeft > 0 ?
-    `Next chunk will be downlaoded in ${(timeLeft / 1000).toFixed(1)} seconds...` :
+    `Next chunk will be downloaded in ${(timeLeft / 1000).toFixed(1)} seconds...` :
     'Next chunk downloading NOW!'
   );
   if (timeLeft > 0) setTimeout(() => showTillNextUpdate(timeLeft - step), step);
